bd12396a57d4e7acbdf80a25d2ccf26a0da560cd
IOhannes m zmölnig <zmoelnig@iem.at>
2012-05-23 10:39:10 +0200

    allow different test-modi (valgrind)

--- build/autotools/tests/runtest.sh
+++ build/autotools/tests/runtest.sh
@@ -4,4 +4,37 @@ EXTERNAL=$1
 EXTERNAL=${EXTERNAL%.la}
 EXTERNAL=${EXTERNAL#./}
 
-pd -nrt -nogui -path .libs -lib ${EXTERNAL}
+TESTTYPE=$2
+
+
+##################################
+
+PD=pd
+#VALGRIND=valgrind
+VALGRIND="valgrind --error-exitcode=1"
+
+do_runtest() {
+case "$1" in
+ mem*|MEM*)
+    ${VALGRIND} ${PD} -nrt -nogui -path .libs -lib ${EXTERNAL}
+ ;;
+ DRD|drd)
+    ${VALGRIND} --tool=drd ${PD} -nrt -nogui -path .libs -lib ${EXTERNAL}
+ ;;
+ HEL*|hel*)
+    ${VALGRIND} --tool=helgrind ${PD} -nrt -nogui -path .libs -lib ${EXTERNAL}
+ ;;
+ *)
+    ${PD} -nrt -nogui -path .libs -lib ${EXTERNAL}
+ ;;
+esac
+}
+
+
+#do_runtest
+#do_runtest MEM
+#do_runtest DRD
+#do_runtest HEL
+do_runtest && do_runtest MEM && do_runtest DRD 
+
+#do_runtest $TESTTYPE
