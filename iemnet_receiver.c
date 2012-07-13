2891f2ad4759ed76f639e74a67df0e53a5987c75
IOhannes m zmoelnig <zmoelnig@umlautO.umlaeute.mur.at>
2012-07-13 11:31:16 +0200

    pthread_create() returns 0 on success(!)

--- iemnet_receiver.c
+++ iemnet_receiver.c
@@ -256,15 +256,13 @@ t_iemnet_receiver*iemnet__receiver_create(int sock, void*userdata, t_iemnet_rece
     /* start the newdata-signalling thread */
     pthread_mutex_lock(&rec->newdata_mtx);
     res=pthread_create(&rec->sigthread, 0, iemnet__receiver_newdatathread, rec);
-    if(res)
-      pthread_cond_wait(&rec->newdata_cond, &rec->newdata_mtx);
+    if(!res)pthread_cond_wait(&rec->newdata_cond, &rec->newdata_mtx);
     pthread_mutex_unlock(&rec->newdata_mtx);
 
     /* start the recv thread */
     pthread_mutex_lock(&rec->running_mtx);
     res=pthread_create(&rec->recthread, 0, iemnet__receiver_readthread, rec);
-    if(res)
-      pthread_cond_wait(&rec->running_cond, &rec->running_mtx);
+    if(!res)pthread_cond_wait(&rec->running_cond, &rec->running_mtx);
     pthread_mutex_unlock(&rec->running_mtx);
   }
   //fprintf(stderr, "new receiver created\n");
