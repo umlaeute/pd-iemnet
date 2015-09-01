7264f8fb619d3cb50873a0d2b449e1cd576e9ffa
IOhannes m zmölnig <zmoelnig@umlautQ.umlaeute.mur.at>
2015-09-01 21:16:44 +0200

    removed unused code

--- iemnet_sender.c
+++ iemnet_sender.c
@@ -209,7 +209,6 @@ void iemnet__sender_destroy(t_iemnet_sender*s, int subthread)
 
   queue_finish(s->queue);
   DEBUG("queue finished");
-  //iemnet__closesocket(sockfd);
 
   pthread_join(s->thread, NULL);
   DEBUG("thread joined");
