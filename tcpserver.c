9168eba51bcdf41ea5ebae77f3557c60afd911f6
IOhannes m zmölnig <zmoelnig@iem.at>
2021-06-07 11:47:09 +0200

    fix typo

--- tcpserver.c
+++ tcpserver.c
@@ -277,7 +277,7 @@ static void tcpserver_info(t_tcpserver *x)
   iemnet__socket2addressout(sockfd, x->x_statusout, gensym("local_address"));
   SETFLOAT(output_atom+0, port);
   outlet_anything( x->x_statusout, gensym("port"), 1, output_atom);
-  SETFLOAT(output_atom+0, x->maxconnections);
+  SETFLOAT(output_atom+0, x->x_maxconnections);
   outlet_anything( x->x_statusout, gensym("maxconnections"), 1, output_atom);
 }
 
